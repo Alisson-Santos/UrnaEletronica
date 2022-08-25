ed044bf3ab10961ca8c907e9a215b9c7a6a553c7
Alisson Santos <alissonxspx98@gmail.com>
2022-08-24 21:43:35 -0300

    scrpt

---
+++ etapas.js
@@ -0,0 +1,49 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR' ,
+        numeros: 5, 
+        candidatos: [
+        {
+            numero: 38111,
+            nome: 'Fulando de Tal'
+            partido: 'ABC', 
+            fotos [
+            {url: '38111.jpg', legenda: 'Vereador', small: false}
+            ]
+        },
+        {
+            numero: 77222,
+            nome: 'beltrano de Tal'
+            partido: 'defg', 
+            fotos [
+            {url: '77222.jpg', legenda: 'Vereador'}
+            ]
+        },
+       ]
+    },
+    {
+        titulo: 'PREFEITO' ,
+        numeros: 2, 
+        candidatos: [
+        {
+            numero: 99,
+            nome: 'ciclano'
+            partido: 'ABC', 
+            fotos [
+                {url: '99.jpg', legenda: 'Vereador'},
+                {url: '99_2.jpg', legenda: 'Vereador', small: false}
+            ]
+        },
+        {
+            numero: 84,
+            nome: 'zulano'
+            partido: 'qwert', 
+            fotos [
+                {url: '84.jpg', legenda: 'Vereador'},
+                {url: '84_2.jpg', legenda: 'Vereador', small: false}
+            ]
+        },
+       ]
+    }
+
+]
