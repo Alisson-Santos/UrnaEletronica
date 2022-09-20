e275ceebb552375a01db46e9a34857cc8131a7b0
Alisson Santos <alissonxspx98@gmail.com>
2022-09-19 22:27:54 -0300

    Terminando projeto

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let numeros = document.querySelector('.d-1-3');
 let etapaAtual = 0;
 let numero = '';
 let votoBranco = false;
+let votos = [] ;
 
 
 function comecarEtapa() {
@@ -96,21 +97,29 @@ function confirma() {
     let etapa = etapas[etapaAtual];
      
     let votoConfirmado = false;
+
     if(votoBranco === true){
         votoConfirmado = true;
-        console.log("Confirmando como BRANCO...");
-    }else {
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        });
+    }else if(numero.length === etapa.numeros){
         votoConfirmado = true;
-        console.log("Confirmando como "+numero);
-    }
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        });
+     }
 
     if(votoConfirmado){
         etapaAtual++;
         if(etapas[etapaAtual] !== undefined) 
         { comecarEtapa();}
-        else{
+    else{
             document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>'
-     }
+            console.log(votos);
+        }
 }
 }
 comecarEtapa();
